83a48a9c9d5eb8d4376c003df28f624aa4d86fed
Rey <reymainmail@gmail.com>
2021-04-29 16:08:39 -0700

    Initial Commit2

---
+++ src/main/java/me/rey/smp/SMPRPG.java
@@ -0,0 +1,18 @@
+package me.rey.smp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SMPRPG extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
